d7be67922eea71acf3a3e34ad0c2d166a1bb18ae
Austin Grech <grech.austin@yahoo.ca>
2023-05-21 23:17:46 -0400

    updated code

--- server.js
+++ server.js
@@ -86,6 +86,6 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "/public/index.html"));
 });
 
-app.listen(PORT, () => {
+app.listen(process.argv.PORT || PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
